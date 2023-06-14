d4a226c799aa89867ee51d5b1b24be03f6872557
Yessalim Salazar <121966068+yessala@users.noreply.github.com>
2023-06-14 18:50:57 -0300

    terminado ejercicio 4

--- POO_Ejer_4/src/principal/Principal.java
+++ POO_Ejer_4/src/principal/Principal.java
@@ -8,11 +8,14 @@ public class Principal {
 	public static void main(String[] args) {
 		ServicioRectangulo sr = new ServicioRectangulo();
 		Rectangulo r = sr.crearRectangulo();
-		
-		sr.superficie(r, r);
-		sr.perimetro(r, r);	
-		
 		System.out.println(r);
+		System.out.println();
+		sr.superficie(r, r);
+		System.out.println();
+		sr.perimetro(r, r);	
+		System.out.println();
+		System.out.println("Dibujando el rectángulo");
+		sr.dibujar(r, r);
 		
 	}
 
--- POO_Ejer_4/src/servicio/ServicioRectangulo.java
+++ POO_Ejer_4/src/servicio/ServicioRectangulo.java
@@ -5,31 +5,56 @@ import java.util.Scanner;
 import entidad.Rectangulo;
 
 public class ServicioRectangulo {
-	
+
 	private Scanner leer = new Scanner(System.in);
 
-public Rectangulo crearRectangulo() {
-		
+	public Rectangulo crearRectangulo() {
+
 		Rectangulo r = new Rectangulo();
-		
+
 		System.out.println("Ingrese la base del rectángulo");
 		r.setBase(leer.nextInt());
 		System.out.println("Ingrese la altura del rectángulo");
 		r.setAltura(leer.nextInt());
-		
+
 		return r;
 	}
-	
+
 	public void superficie(Rectangulo b, Rectangulo a) {
-		
+
 		System.out.println("La superficie del rectángulo es " + (b.getBase()*a.getAltura()));
-		
+
 	}
-	
+
 	public void perimetro (Rectangulo b, Rectangulo a) {
-		
+
 		System.out.println("El perimetro del rectángulo es " + (b.getBase()*a.getAltura()*2));
-		
+
 	}
-	
+
+	public void dibujar(Rectangulo b, Rectangulo a) {
+
+		//Linea superior
+		for (int i = 0; i < b.getBase(); i++) {
+			System.out.print("* ");
+		}
+		System.out.println();//salto de línea
+
+		//Lineas centro
+		for (int i = 0; i < a.getAltura()-2; i++) {
+			System.out.print("* ");
+			for (int j = 0; j < b.getBase()-2; j++) {
+				System.out.print("  ");//espacio vacio
+			}
+			System.out.println("*");
+		}
+
+		//Linea inferior
+		for (int i = 0; i < b.getBase(); i++) {
+			System.out.print("* ");
+
+		} 
+
+
+	}
 }
